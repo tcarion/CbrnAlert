3406b3bba9a37f3c6187d83cd4a13de5bfd1b6cf
tcarion <tristancarion@gmail.com>
2020-12-18 09:25:27 +0000

    remove test function in routes

--- routes.jl
+++ routes.jl
@@ -8,8 +8,6 @@ route("/", ATPController.preloaded_data, named = :preloaded_data)
 
 route("/load/:file::String", ATPController.preloaded_data, named = :preloaded_data_picked)
 
-route("/echo/:message", ATPController.echom)
-
 route("/archive_data", ATPController.archive_data, named = :archive_data)
 
 route("/atp_coords", ATPController.shape_coord_request, method = POST, named = :atp_coords)
