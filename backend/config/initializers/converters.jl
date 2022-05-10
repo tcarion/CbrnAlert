71a08a4165a7f7f89e711d136ee0de6bd1af091c
tcarion <tristancarion@gmail.com>
2022-05-10 15:11:31 +0000

    [backend] change some default files

--- backend/config/initializers/converters.jl
+++ backend/config/initializers/converters.jl
@@ -1,6 +1,6 @@
 using Dates
 import Base.convert
 
-# Base.convert(::Type{Int}, v::SubString{String}) = parse(Int, v)
-# Base.convert(::Type{Float64}, v::SubString{String}) = parse(Float64, v)
-# Base.convert(::Type{Date}, s::String) = parse(Date, s)
+Base.convert(::Type{Int}, v::SubString{String}) = parse(Int, v)
+Base.convert(::Type{Float64}, v::SubString{String}) = parse(Float64, v)
+Base.convert(::Type{Date}, s::String) = parse(Date, s)
