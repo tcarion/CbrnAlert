22c396f58916c8ae0d823f466f8b68fc2aaf8c5a
tcarion <tristancarion@gmail.com>
2022-05-24 09:35:00 +0000

    clean routes comments

--- backend/routes.jl
+++ backend/routes.jl
@@ -11,7 +11,6 @@ using UUIDs
 using YAML
 
 using SwagUI
-# using SwaggerMarkdown
 
 using Genie.Cache
 Genie.config.cache_duration = 3600
@@ -22,31 +21,9 @@ using Users
 
 
 Genie.config.websockets_server = true
-# Genie.Cache.init()
-# StructTypes.StructType(::Type{Genie.WebChannels.ChannelMessage}) = StructTypes.Struct()
 for user in SearchLight.all(User)
     Genie.Assets.channels_subscribe(user.username)
 end
-
-atp45_routes = Dict(
-    "available_steps" => Atp45Controller.available_steps,
-    "available_gribfiles" => Atp45Controller.available_grib_files,
-    "prediction_request" => Atp45Controller.prediction_request,
-    "archive_retrieval" => Atp45Controller.archive_retrieval,
-    # "realtime_available_steps" => Atp45Controller.realtime_available_steps,
-    "realtime_prediction_request" => Atp45Controller.realtime_prediction_request,
-)
-
-flexpart_routes = Dict(
-    # "metdata_retrieval" => FlexpartController.flexextract_request,
-    # "available_flexpart_input" => FlexpartController.available_flexpart_input,
-    # "flexpart_run" => FlexpartController.flexpart_run,
-    # "flexpart_options" => FlexpartController.flexpart_options,
-    # "flexpart_results" => FlexpartController.flexpart_results,
-    # "flexpart_conc" => FlexpartController.flexpart_conc,
-    # "flexpart_geojson_conc" => FlexpartController.flexpart_geojson_conc,
-    # "flexpart_daily_average" => FlexpartController.flexpart_daily_average,
-)
 
 api_routes = Dict(
     "/forecast/available" => (f=Atp45Controller.available_steps, keyargs=(method=GET,)),
@@ -58,16 +35,11 @@ api_routes = Dict(
     "/flexpart/run" => (f=FlexpartController.flexpart_run, keyargs=(method=POST, named=:flexpart_run)),
     "/flexpart/runs" => (f=FlexpartController.get_runs, keyargs=(method = GET,)),
     "/flexpart/runs/:runId::String" => (f=FlexpartController.get_run, keyargs=(method = GET,)),
-    # "/flexpart/result/:result_id::String" => (f=FlexpartController.get_result, keyargs=(method=GET, named=:get_flexpart_result)),
     "/flexpart/runs/:runId::String/outputs" => (f=FlexpartController.get_outputs, keyargs=(method = GET,)),
-    # "/flexpart/results/:result_id::String/outputs" => (f=FlexpartController.get_outputs, keyargs=(method = GET, named = :get_flexpart_outputs)),
     "/flexpart/runs/:runId::String/outputs/:outputId::String" => (f=FlexpartController.get_output, keyargs=(method = GET, named = :get_flexpart_output)),
     "/flexpart/outputs/:outputId::String/layers/" => (f=FlexpartController.get_layers, keyargs=(method = GET,)),
     "/flexpart/outputs/:outputId::String/dimensions/" => (f=FlexpartController.get_dimensions, keyargs=(method = GET,)),
     "/flexpart/outputs/:outputId::String/slice/" => (f=FlexpartController.get_slice, keyargs=(method = POST,)),
-    # "/flexpart/results/:result_id::String/outputs/:output_id::String" => (f=FlexpartController.get_output, keyargs=(method = GET, named = :get_flexpart_output)),
-    # "/flexpart/results/:result_id::String/output/:output_id::String" => (f=FlexpartController.get_plot, keyargs=(method = POST,)),
-    # "/flexpart/results/:result_id::String/output/:output_id::String/daily_average" => (f=FlexpartController.daily_average, keyargs=(method = POST,))
 )
 
 for (url, args) in api_routes
@@ -77,79 +49,9 @@ for (url, args) in api_routes
     end
 end
 
-# openApi_version = "2.0"
-
-# # the info of the API, title and version of the info are required
-# info = Dict{String, Any}()
-# info["title"] = "CBRN Dispersion modeling API"
-# info["version"] = "1.0.0"
-
-# openApi = OpenAPI(openApi_version, info)
-
-# swagger_document = build(openApi)
-# swagger_json = JSON.json(swagger_document)
-
-# # @swagger """
-# # /api/flexpart/meteo_data_request:
-# #     post:
-# #         description: Run a flex_extract retrieval.
-# #         responses:
-# #         '200':
-# #             description: OK.
-# # """
-# @swagger """
-# /doge:
-#   get:
-#     description: Doge to the moon!
-#     responses:
-#       '200':
-#         description: Doge to the moon!!.
-# """
-
 route("/docs") do 
     swagger_document = YAML.load_file("api_docs.yaml"; dicttype=Dict{String, Any})
     render_swagger(swagger_document)
-end
-
-# route("/api/flexpart/results", method = GET, FlexpartController.get_results)
-# route("/api/flexpart/results/:result_id::String/output/:output_id::String", method = GET, FlexpartController.get_plot)
-
-# route("/api/flexpart/meteo_data_request", method = POST, FlexpartController.meteo_data_request, named = :meteo_data_request)
-# route("/api/flexpart/results", method = GET, FlexpartController.get_results, named = :get_flexaprt_results)
-# route("/api/flexpart/results/:result_id::String", method = GET, FlexpartController.get_result, named = :get_flexpart_result)
-
-
-# route("/api/flexpart/results/:result_id::String/outputs", method = GET, FlexpartController.get_outputs, named = :get_flexpart_outputs)
-# route("/api/flexpart/results/:result_id::String/outputs/:output_id::String", method = GET, FlexpartController.get_output, named = :get_flexpart_output)
-
-# route("/api/flexpart/results/:result_id::String/output/:output_id::String", method = POST, FlexpartController.get_plot)
-# route("/api/flexpart/results/:result_id::String/output/:output_id::String/daily_average", method = POST, FlexpartController.daily_average)
-# route("/api/flexpart/results/:result_id", method = POST) do
-#     println(params(:result_id))
-# end
-# route("/api/flexpart/results/:result_id::String/:output_id::String", method = GET, FlexpartController.get_output)
-#     notauth = AuthenticationController.isauth()
-#     !isnothing(notauth) && return notauth
-#     # println(get(params, :test, ""))
-#     # println(params(:test))
-#     readdir(FLEXPART_RUNS_DIR) |> Genie.Renderer.Json.json
-# end
-
-route("/atp45", method = POST) do
-    process_request(atp45_routes)
-end
-# HTTP.request("POST", "http://localhost:8000/flexpart", [("Content-Type", "application/json")], """{"request":"flexpart_results"}""")
-route("/flexpart", method = POST) do
-    process_request(flexpart_routes)
-end
-
-function process_request(routes)
-    notauth = AuthenticationController.isauth()
-    !isnothing(notauth) && return notauth
-    payload = jsonpayload()
-    request = payload["request"]
-    @info request
-    haskey(routes, request) ? Genie.Renderer.Json.json(routes[request](payload)) : Genie.Router.error(404, "Request not found")
 end
 
 route("/login", AuthenticationController.login, method = POST)
