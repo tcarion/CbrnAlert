6e15788c9f71c6883c2da005b6ea1f446aa88cdb
tcarion <tristancarion@gmail.com>
2022-11-17 10:36:08 +0000

    change bootstrap for julia v1.8

--- backend/bootstrap.jl
+++ backend/bootstrap.jl
@@ -1,5 +1,5 @@
 (pwd() != @__DIR__) && cd(@__DIR__) # allow starting app from bin/ dir
 
 using CbrnAlertApp
-push!(Base.modules_warned_for, Base.PkgId(CbrnAlertApp))
+const UserApp = CbrnAlertApp
 CbrnAlertApp.main()
