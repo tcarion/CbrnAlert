d4fadb033be530568f4ac5d06afa9aff23c1a43d
tcarion <tristancarion@gmail.com>
2023-02-16 11:13:07 +0000

    new add user seed

--- backend/db/seeds/AddUser.jl
+++ backend/db/seeds/AddUser.jl
@@ -1,8 +1,10 @@
 using Users
 using SearchLight
 
-function add_user(username, password)
-    user = User(username  = username,
-                password  = password |> Users.hash_password,
-                ) |> SearchLight.save!
+function add_user(email, password, username)
+    user = User(
+        email  = email,
+        username = username,
+        password  = password |> Users.hash_password,
+    ) |> SearchLight.save!
 end
