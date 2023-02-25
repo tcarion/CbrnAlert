879bddec141cc5b8e74fefd950edd0ef1877c939
tcarion <tristancarion@gmail.com>
2023-02-25 13:10:32 +0000

    remove comments in map plot list

--- frontend/src/app/shared/map-plot-list/map-plot-list/map-plot-list.component.ts
+++ frontend/src/app/shared/map-plot-list/map-plot-list/map-plot-list.component.ts
@@ -1,9 +1,7 @@
 import { MapPlotState } from './../../../core/state/map-plot.state';
 import { Observable, Subject } from 'rxjs';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 import { MapPlot } from 'src/app/core/models/map-plot';
-import { MapPlotsService } from 'src/app/core/services/map-plots.service';
-import { map, takeUntil } from 'rxjs/operators';
 import { Select, Store } from '@ngxs/store';
 import { MapPlotAction } from 'src/app/core/state/map-plot.state';
 
@@ -12,38 +10,21 @@ import { MapPlotAction } from 'src/app/core/state/map-plot.state';
 @Component({
     selector: 'app-map-plot-list',
     templateUrl: './map-plot-list.component.html',
-    styleUrls: ['./map-plot-list.component.scss']
-})
+    styleUrls: ['./map-plot-list.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+  })
 
-export class MapPlotListComponent implements OnInit, OnDestroy {
-    // private readonly onDestroy = new Subject<void>();
-
+export class MapPlotListComponent {
     @Select(MapPlotState.filterType('atp45')) atp45Plots$: Observable<MapPlot[]>
     @Select(MapPlotState.filterType('flexpart')) flexpartPlots$: Observable<MapPlot[]>
-    // flexpartPlots$: Observable<MapPlot[]> = this.mapPlotsService.mapPlots$.pipe(map((plots) => plots.filter((plot) => plot.type === 'flexpart')));
-
-    // flexpartPlots: MapPlot[] = [];
 
     constructor(
         private store: Store,
     ) { }
 
-    ngOnInit(): void {
-        // this.sortPlots(this.mapPlotsService.plots);
-
-        // this.mapPlotsService.newAtp45Plot()
-        //     .pipe(takeUntil(this.onDestroy))
-        //     .subscribe();
-    }
-
-    // sortPlots(plots: MapPlot[]) {
-    //     this.atp45Plots = plots.filter((plot: any) => plot.type === 'atp45');
-    //     this.flexpartPlots = plots.filter((plot: any) => plot.type === 'flexpart');
-    // }
-
     toggleVisibility(plot: MapPlot) {
         plot.visible ? this.store.dispatch(new MapPlotAction.Hide(plot.id)) : this.store.dispatch(new MapPlotAction.Show(plot.id));
-        
+
     }
 
     setActive(plotId: number) {
@@ -51,13 +32,6 @@ export class MapPlotListComponent implements OnInit, OnDestroy {
     }
 
     delete(plotId: number) {
-        // this.mapPlotsService.deleteMapPlot(plot);
         this.store.dispatch(new MapPlotAction.Remove(plotId));
-        // this.sortPlots(this.mapPlotsService.plots);
-    }
-
-    ngOnDestroy() {
-        // this.onDestroy.next();
-        // this.onDestroy.complete();
     }
 }
