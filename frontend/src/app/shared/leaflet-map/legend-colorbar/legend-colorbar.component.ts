f698a73c92f067e0d2c20c39e9f126558493f5e2
PrzPaul <paullouisperez@gmail.com>
2024-10-15 12:31:43 +0000

    fixed unit for deposition in multi-release case + cleanup

--- frontend/src/app/shared/leaflet-map/legend-colorbar/legend-colorbar.component.ts
+++ frontend/src/app/shared/leaflet-map/legend-colorbar/legend-colorbar.component.ts
@@ -28,17 +28,7 @@ export class LegendColorbarComponent implements OnInit {
   @Input() set colorbar(value: ColorbarData) {
     this.formatedTicks = value.ticks.map(i => this.formatTick(i));
     this.colors = value.colors;
-    //this.unit = value.unit;
-    // set unit based on the output variable
-      //this.setUnit(this.layerName);
-      //this.updateLegend(this.layerName)
-    // this.units = value.units;
   }
-
-  // ticksLabels: string[];
-  // colors: string[];
-  // ticksLabels$: Observable<string[]>;
-  // colors$: Observable<string[]>;
 
   constructor(
     private flexpartService: FlexpartService,
@@ -55,7 +45,6 @@ export class LegendColorbarComponent implements OnInit {
       if (layerName) {
         this.layerName = layerName;
         this.updateLegend(layerName);
-        //this.unit = layerName; // for priority when multiple plots
       }
     });
     
@@ -66,19 +55,10 @@ export class LegendColorbarComponent implements OnInit {
           this.updateLegend(this.activePlot.legendLayer);
       }
       this.cdr.markForCheck();
-  });
-
-
-
-
-    // this.ticksLabels$ = of(this.colorbar).pipe(map(cb => cb.ticks!.map((e: number) => { return e.toExponential(2) })))
-    // this.ticksLabels = this.colorbar.ticks!.map((e: number) => { return e.toExponential(2) });
-    // this.colors = <string[]>this.colorbar.colors
-    // this.colors$ = of(this.colorbar).pipe(map(cb => cb.colors as string[]))
+    });
   }
 
   formatTick(tick:number) {
-    //return tick.toExponential(2)
     return String(tick)
   }
 
@@ -89,10 +69,10 @@ export class LegendColorbarComponent implements OnInit {
       this.unit = 'm';
     } else if (/spec\d+_mr/.test(layerName)) {
       this.unit = 'ng/m³';
-    } else if (['WD_spec001', 'DD_spec001', 'TD_spec001'].includes(layerName)) {
+    } else if (/D_spec/.test(layerName)) {
       this.unit = 'pg/m²';
     } else {
-      this.unit = 'Click plot again';
+      this.unit = 'No unit';
       console.log("why no units ? " + layerName)
     }
   }
@@ -107,6 +87,5 @@ export class LegendColorbarComponent implements OnInit {
     console.log("new unit : ", this.unit)
     this.cdr.markForCheck();
   }
-
 
 }
