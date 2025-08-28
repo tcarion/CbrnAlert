8c599cc43f8ad991781548859907f48ede00ccec
PrzPaul <paullouisperez@gmail.com>
2025-08-28 16:00:42 +0000

    temporary fix for flex_extract bug for ensemble data retrieval

--- backend/setup.jl
+++ backend/setup.jl
@@ -1,3 +1,8 @@
+# this fix is due to a bug in flex_extract when retrieving ensemble data. When devs fix it, it can be removed.
+using FlexExtract
+faulty_script = joinpath(FlexExtract.PATH_FLEXEXTRACT, "Source", "Python", "Mods", "checks.py")
+run(`sed -i "882s@'{:0>3}'.join(numbers)@'/'.join(numbers)@" $faulty_script`)
+
 if isfile(joinpath("db", "db.sqlite"))
     println("db exists, skipping initialization")
     exit()
